1d006ab6cb3059eb78f14157e8b969cccb6ab56f
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-03-13 10:50:39 +0100

    setup a new entry point for the project

--- GetItDone/App Files/App Delegates/SceneDelegate.swift
+++ GetItDone/App Files/App Delegates/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let rootViewController = ViewController()
+        let rootViewController = TasksListViewController()
         rootViewController.navigationItem.title = "Get It Done"
         
         let navigationController = UINavigationController(rootViewController: rootViewController)
---
+++ GetItDone/Tasks/TasksListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TasksListViewController.swift
+//  GetItDone
+//
+//  Created by waheedCodes on 13/03/2022.
+//
+
+import UIKit
+
+final class TasksListViewController: UIViewController {
+    
+    // MARK: - Object Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+}
